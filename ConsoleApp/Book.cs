a772c0014817069e805bef6f99f9a85d239552ba
Oleh Berehovskyi <berehovskyi@gmail.com>
2019-10-11 01:24:43 +0300

    0.0.1 init

---
+++ ConsoleApp/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp {
+
+    public struct Book {
+
+        public string Title;
+        public string Name;
+        public string Author;
+
+    }
+
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex32;
+
+namespace ConsoleApp {
+
+    static class Program {
+
+        static void Main(string[] args) {
+            var startNew = Stopwatch.StartNew();
+            startNew.Start();
+            Matrix<double> matrixC;
+            {
+                var matrixA = Matrix<double>.Build.Random(3000, 4000);
+                var matrixB = Matrix<double>.Build.Random(4000, 3000);
+                matrixC = matrixA * matrixB;
+            }
+            startNew.Stop();
+            Console.WriteLine(matrixC);
+            Console.WriteLine(startNew.Elapsed);
+            // 21 sec
+        }
+
+    }
+
+}
